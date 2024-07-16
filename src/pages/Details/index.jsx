13f4c592eef366c7f46f69d390ec87ad02d1cde0
Yuri Sousa <ysousa.dev@gmail.com>
2024-07-16 12:17:12 -0300

    feat: :sparkles: (treinando) aplica alguns conceitos básico de styled-components

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles"
+import { Fragment } from "react"
+
+export function Details() {
+  return(
+    <Container>
+      <h1>Hello World!</h1>
+    </Container>
+  )
+}
