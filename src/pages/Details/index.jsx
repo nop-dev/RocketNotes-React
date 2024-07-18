ff66d18458a378f3d6f4e86c78a1156a3ad4a74a
Yuri Sousa <ysousa.dev@gmail.com>
2024-07-18 11:32:17 -0300

    feat: :sparkles: adiciona pequenas alterações, e temas. aprendendo a estilizar de forma global...

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,10 @@
 import { Container } from "./styles"
-import { Fragment } from "react"
 
 export function Details() {
   return(
-    <Container>
-      <h1>Hello World!</h1>
-    </Container>
+        <Container>
+            <h1>Hello World!</h1>
+            <span>teste</span>
+        </Container>    
   )
 }
