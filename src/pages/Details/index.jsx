111de51a069dbf39337102362754b95bf2330eca
Yuri Sousa <ysousa.dev@gmail.com>
2024-07-19 09:15:03 -0300

    feat: :sparkles: adiciona propriedades aos botões
    Nessa aula estudei um pouco melhor sobre como que funcionam as propriedades, apliquei alguns conceitos disso aqui

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,15 @@
 import { Container } from "./styles"
+import { Button } from "../../components/Button"
 
 export function Details() {
+
   return(
         <Container>
             <h1>Hello World!</h1>
             <span>teste</span>
+            <Button title="Olá" loading />
+            <Button title="td bem?" loading />
+            <Button title="Sim?" />
         </Container>    
   )
 }
