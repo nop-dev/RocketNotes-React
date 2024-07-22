61521c69a16561bdd42a61a35e32defbd0926686
Yuri Sousa <ysousa.dev@gmail.com>
2024-07-22 08:18:08 -0300

    feat: :sparkles: cria e aplica um componente de tags

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,8 +1,9 @@
-import { Container, Links } from "./styles"
+import { Container, Links } from "./styles";
 
-import { Button } from "../../components/Button"
-import { Header } from "../../components/Header"
-import { Section } from "../../components/Section"
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tags";
 
 export function Details() {
 
@@ -16,8 +17,13 @@ export function Details() {
                   <li><a href="#">https://www.rocketseat.com.br/</a></li>
               </Links>
             </Section>
+
+            <Section title="Marcadores">
+              <Tag title="express"/>
+              <Tag title="nodejs"/>
+            </Section>
             
             <Button title="Voltar" />
         </Container>    
-  )
-}
+  );
+};
