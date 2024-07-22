2296076c9e7575535cad7ac66135582388f5ca33
Yuri Sousa <ysousa.dev@gmail.com>
2024-07-22 08:29:06 -0300

    feat: :sparkles: adiciona componente ButtonText e o aplica em Details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -4,13 +4,16 @@ import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
 import { Tag } from "../../components/Tags";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
 
   return(
         <Container>
             <Header />
-
+            
+            <ButtonText title="Excluir a nota" />
+            
             <Section title="Links Úteis">
               <Links>
                   <li><a href="#">https://www.rocketseat.com.br/</a></li>
