54641b0314c228818db70683b927fb1932acd037
Yuri Sousa <ysousa.dev@gmail.com>
2024-07-23 10:09:25 -0300

    feat: :sparkles: adiciona botão de nova nota na página home

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -54,4 +54,13 @@ export const Content = styled.div`
 
 export const NewNote = styled.button`
   grid-area: newnote;
+
+  background-color: ${({ theme }) => theme.COLORS.ORANGE};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  > svg {
+    margin-right: 8px;
+  }
 `;
