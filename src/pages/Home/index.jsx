bc07c5ef41fa415736b02d1c23b0b84df8b507c9
Yuri Sousa <ysousa.dev@gmail.com>
2024-07-30 09:00:21 -0300

    feat: :sparkles: adiciona componente de notas e finaliza a página home (front-end)

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,8 @@ import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+import { Note } from '../../components/Note';
 import { ButtonText } from '../../components/ButtonText'
 import { Input } from '../../components/Input';
 
@@ -22,11 +24,21 @@ export function Home() {
       </Menu>
 
       <Search>
-        <Input placeholder="Pesquisar pelo título" icon={FiSearch } />
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
       </Search>
 
       <Content>
+        <Section title="Minhas Notas">
 
+          <Note data = {{
+            title:  "React",
+            tags: [
+                    { id: 1, name: 'react'},
+                    { id: 2, name: 'js'}
+                  ]
+          }} />
+
+        </Section>
       </Content>
 
       <NewNote>
